f9ec805173a6171da7f0d35ebb2b21a15aeafa5c
solivan <solivan_lau@outlook.com>
2024-02-05 17:02:59 -0500

    basic sass set up, boiler plate removed, aliases set up for import

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-
-// https://vitejs.dev/config/
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 export default defineConfig({
-  plugins: [react()],
-})
+	plugins: [react()],
+	resolve: {
+		alias: {
+			src: "/src",
+			assets: "/src/assets",
+			components: "/src/components",
+			styles: "/src/styles",
+		},
+	},
+});
